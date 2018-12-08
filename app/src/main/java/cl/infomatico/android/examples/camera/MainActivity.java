3bc3bcfb98a77875afe81a10b0dee9f2e2e54b8e
Yerko Mora <morayerko@gmail.com>
2018-12-08 14:21:46 -0300

    Added requesting dynamics permissions.

--- app/src/main/java/cl/infomatico/android/examples/camera/MainActivity.java
+++ app/src/main/java/cl/infomatico/android/examples/camera/MainActivity.java
@@ -1,33 +1,29 @@
 package cl.infomatico.android.examples.camera;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private static final String ERROR_STARTING_CAMERA = "Error starting camera: ";
 
-    private Camera mCamera;
+    // AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (checkCameraHardware(getBaseContext())) {
-            mCamera = getCameraInstance();
-
-            // Preview
-
-            CameraPreview mPreview = new CameraPreview(this, mCamera);
-            FrameLayout preview = findViewById(R.id.camera_preview);
-            preview.addView(mPreview);
-        }
+        if (checkCameraHardware(getBaseContext()))
+            cameraPermissionsCheck();
     }
 
     @Override
@@ -35,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         mCamera.stopPreview();
     }
+
+    // MainActivity
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // Camera
+
+    private static final String ERROR_STARTING_CAMERA = "Error starting camera: ";
+    private Camera mCamera;
 
     private boolean checkCameraHardware(Context context) {
         return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
@@ -48,5 +53,52 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, ERROR_STARTING_CAMERA);
         }
         return c;
+    }
+
+    // Permissions
+
+    private static final int PERMISSIONS_CAMERA = 1;
+
+    private void cameraPermissionsCheck() {
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                builder.setMessage(R.string.permission_camera);
+                builder.setTitle(R.string.permission_required);
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        cameraPermissionsMake();
+                    }
+                });
+
+                builder.create().show();
+            } else {
+                cameraPermissionsMake();
+            }
+        } else {
+            cameraOpen();
+        }
+    }
+
+    private void cameraPermissionsMake() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                PERMISSIONS_CAMERA);
+    }
+
+    private void cameraOpen() {
+        mCamera = getCameraInstance();
+
+        // Preview
+
+        CameraPreview mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
     }
 }
